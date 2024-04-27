7d8260f3f0da2eef771db816e6330e936a467c28
hasakisi <tunayylz22@gmail.com>
2024-04-27 22:25:19 +0300

    Add project files.

---
+++ okul_yonetim/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using okul_yonetim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace okul_yonetim.Controllers
+{
+    public class HomeController : Controller
+    {
+        Database1Entities1 db = new Database1Entities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Yonetici log)
+        {
+            var user = db.Yonetici.FirstOrDefault(x => x.YoneticiAd == log.YoneticiAd && x.YoneticiSoyad == log.YoneticiSoyad);
+
+            if (user != null)
+            {
+                Session["AdminName"] = user.YoneticiAd;
+                Session["AdminName"] = user.YoneticiAd;
+
+                return View("Dashboard");
+            }   
+
+            else
+            {
+                return View();
+            }
+        }   
+        public ActionResult Logout()
+        {
+            Session.Clear();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+           
+            return View();
+        }
+
+    
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ okul_yonetim/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="okul_yonetim.MvcApplication" Language="C#" %>
